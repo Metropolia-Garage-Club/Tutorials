8254971db0941c3297ae16cdddbf972ac7ca74fd
Gandlaf <mono.touko@gmail.com>
2025-06-02 17:58:49 +0300

    added script to disable bingsearch on the entire local machinadded script to disable bingsearch on the entire local machinee

--- windows_dev_setup/aiot_win11_setup.bat
+++ windows_dev_setup/aiot_win11_setup.bat
@@ -1,1 +1,4 @@
 Rem This file is used to setup registry keys to remove bing results from windows search
+Rem Run as Admin
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f
+taskkill /f /im explorer.exe & start explorer.exe
