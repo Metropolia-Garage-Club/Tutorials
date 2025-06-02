bce29d2e8fc305b1d94977766084f5dd34328817
Gandlaf <mono.touko@gmail.com>
2025-06-02 17:26:56 +0300

    Created win11 setup tutorial, added intial software install script

---
+++ windows_dev_setup/aiot_win11_setup.bat
@@ -0,0 +1,1 @@
+Rem This file is used to setup registry keys to remove bing results from windows search
---
+++ windows_dev_setup/win11_app_collection.bat
@@ -0,0 +1,79 @@
+Rem This script installs useful software used in the AIoT Garage 
+Rem ### CAD ###
+winget install --id=KiCad.KiCad -e &&
+winget install --id=FreeCAD.FreeCAD -e &&
+winget install --id=GNU.Octave -e &&
+
+Rem ### 3D Printing 
+winget install --id=Prusa3D.PrusaSlicer -e && 
+Rem winget install --id=Mango3D.LycheeSlicer -e && 
+Rem winget install --id=SoftFever.OrcaSlicer -e &&
+Rem winget install --id=Ultimaker.Cura -e &&
+
+Rem Currently not allowed in Metropolia Network!!
+Rem winget install --id=Bambulab.Bambustudio -e &&
+
+Rem Communication Software
+winget install --id=Zoom.Zoom -e &&
+
+Rem ### Media & Document Tools ###
+winget install --id=TheDocumentFoundation.LibreOffice -e &&
+winget install --id=Adobe.Acrobat.Reader.64-bit -e &&
+winget install --id=Audacity.Audacity -e &&
+winget install --id=GIMP.GIMP.3 -e &&
+winget install --id=OBSProject.OBSStudio -e &&
+winget install --id=VideoLAN.VLC -e &&
+
+Rem ### DEV Tools ###
+winget install --id=Notepad++.Notepad++ -e &&
+winget install --id=Microsoft.VisualStudioCode -e && 
+winget install --id=ArduinoSA.IDE.stable -e &&
+winget install --id=Git.Git -e &&
+winget install --id=GitHub.cli -e &&
+winget install --id=GitHub.GitHubDesktop -e &&
+winget install --id=Google.CloudSDK -e &&
+winget install --id=Amazon.AWSCLI -e && 
+winget install --id=Hashicorp.Terraform -e &&
+winget install --id=Python.Python.3.12 -e &&
+winget install --id=Anaconda.Anaconda3 -e &&
+winget install --id=Anaconda.Miniconda3 -e && 
+winget install --id=Docker.DockerDesktop -e &&
+winget install --id=DEVCOM.Lua -e &&
+
+Rem ## Disk Image Tools ##
+winget install --id=Rufus.Rufus -e &&
+winget install --id=RaspberryPiFoundation.RaspberryPiImager -e &&
+
+Rem ## Network and VM
+winget install --id=WireGuard.WireGuard -e &&
+winget install --id=Netbird.Netbird -e && 
+winget install --id=WiresharkFoundation.Wireshark -e &&
+winget install --id=Microsoft.WSL -e && 
+winget install --id=Bostrot.WSLManager -e &&
+winget install --id=Oracle.VirtualBox -e &&
+
+Rem ## Other Useful tools
+winget install --id=Mozilla.Firefox -e &&
+winget install --id=RARLab.WinRAR -e &&
+winget install --id=7zip.7zip -e &&
+winget install --id=KeePassXCTeam.KeePassXC -e &&
+
+winget install --id=Microsoft.PowerToys -e &&
+winget install --id=Henry++.ErrorLookup -e &&
+
+Rem ### Benchark Tools ###
+Rem winget install --id=Maxon.CinebenchR23 -e &&
+
+Rem ### Gaming related applications ###
+Rem winget install --id=GOG.Galaxy -e
+Rem winget install --id=Valve.Steam -e &&
+Rem winget install --id=Nvidia.GeForceNow -e &&
+Rem winget install --id=WhirlwindFX.SignalRgb -e &&
+Rem winget install --id=Wagnardsoft.DisplayDriverUninstaller -e &&
+Rem winget install --id=Guru3D.Afterburner -e &&
+Rem winget install --id=CPUID.HWMonitor -e && 
+Rem winget install --id=TechPowerUp.GPU-Z -e &&
+Rem winget install --id=CPUID.CPU-Z -e &&
+Rem winget install --id=Spotify.Spotify -e &&
+Rem winget install --id=REALiX.HWiNFO -e &&
+Rem winget install --id=Discord.Discord -e &&
