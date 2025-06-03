dff6a0c68b075f3ee03ba88d53ad609b19e6732d
Gandlaf <mono.touko@gmail.com>
2025-06-03 10:48:54 +0300

    commented out software already found on lab computers

--- windows_dev_setup/win11_app_collection.bat
+++ windows_dev_setup/win11_app_collection.bat
@@ -14,19 +14,19 @@ Rem Currently not allowed in Metropolia Network!!
 Rem winget install --id=Bambulab.Bambustudio -e --scope machine &&
 
 Rem Communication Software
-winget install --id=Zoom.Zoom -e --scope machine &&
+Rem winget install --id=Zoom.Zoom -e --scope machine &&
 
 Rem ### Media & Document Tools ###
-winget install --id=TheDocumentFoundation.LibreOffice -e --scope machine &&
-winget install --id=Adobe.Acrobat.Reader.64-bit -e --scope machine &&
+Rem winget install --id=TheDocumentFoundation.LibreOffice -e --scope machine &&
+Rem winget install --id=Adobe.Acrobat.Reader.64-bit -e --scope machine &&
 winget install --id=Audacity.Audacity -e --scope machine &&
 winget install --id=GIMP.GIMP.3 -e --scope machine &&
 winget install --id=OBSProject.OBSStudio -e --scope machine &&
-winget install --id=VideoLAN.VLC -e --scope machine &&
+Rem winget install --id=VideoLAN.VLC -e --scope machine &&
 
 Rem ### DEV Tools ###
-winget install --id=Notepad++.Notepad++ -e --scope machine &&
-winget install --id=Microsoft.VisualStudioCode -e --scope machine && 
+Rem winget install --id=Notepad++.Notepad++ -e --scope machine &&
+Rem winget install --id=Microsoft.VisualStudioCode -e --scope machine && 
 winget install --id=ArduinoSA.IDE.stable -e --scope machine &&
 winget install --id=Git.Git -e --scope machine &&
 winget install --id=GitHub.cli -e --scope machine &&
@@ -34,11 +34,11 @@ winget install --id=GitHub.GitHubDesktop -e --scope machine &&
 winget install --id=Google.CloudSDK -e --scope machine &&
 winget install --id=Amazon.AWSCLI -e --scope machine && 
 winget install --id=Hashicorp.Terraform -e --scope machine &&
-winget install --id=Python.Python.3.12 -e --scope machine &&
-winget install --id=Anaconda.Anaconda3 -e --scope machine &&
-winget install --id=Anaconda.Miniconda3 -e --scope machine && 
+Rem winget install --id=Python.Python.3.12 -e --scope machine &&
+Rem winget install --id=Anaconda.Anaconda3 -e --scope machine &&
+Rem winget install --id=Anaconda.Miniconda3 -e --scope machine && 
 winget install --id=Docker.DockerDesktop -e --scope machine &&
-winget install --id=DEVCOM.Lua -e --scope machine &&
+Rem winget install --id=DEVCOM.Lua -e --scope machine &&
 
 Rem ## Disk Image Tools ##
 winget install --id=Rufus.Rufus -e --scope machine &&
@@ -47,19 +47,19 @@ winget install --id=RaspberryPiFoundation.RaspberryPiImager -e --scope machine &
 Rem ## Network and VM
 winget install --id=WireGuard.WireGuard -e --scope machine &&
 winget install --id=Netbird.Netbird -e --scope machine && 
-winget install --id=WiresharkFoundation.Wireshark -e --scope machine &&
-winget install --id=Microsoft.WSL -e --scope machine && 
-winget install --id=Bostrot.WSLManager -e --scope machine &&
-winget install --id=Oracle.VirtualBox -e --scope machine &&
+Rem winget install --id=WiresharkFoundation.Wireshark -e --scope machine &&
+Rem winget install --id=Microsoft.WSL -e --scope machine && 
+Rem winget install --id=Bostrot.WSLManager -e --scope machine &&
+Rem winget install --id=Oracle.VirtualBox -e --scope machine &&
 
 Rem ## Other Useful tools
 Rem winget install --id=Mozilla.Firefox -e --scope machine &&
-winget install --id=RARLab.WinRAR -e --scope machine &&
-winget install --id=7zip.7zip -e --scope machine &&
-winget install --id=KeePassXCTeam.KeePassXC -e --scope machine &&
+Rem winget install --id=RARLab.WinRAR -e --scope machine &&
+Rem winget install --id=7zip.7zip -e --scope machine &&
+Rem winget install --id=KeePassXCTeam.KeePassXC -e --scope machine &&
 
-winget install --id=Microsoft.PowerToys -e --scope machine &&
-winget install --id=Henry++.ErrorLookup -e --scope machine &&
+Rem winget install --id=Microsoft.PowerToys -e --scope machine &&
+Rem inget install --id=Henry++.ErrorLookup -e --scope machine &&
 
 Rem ### Benchark Tools ###
 Rem winget install --id=Maxon.CinebenchR23 -e --scope machine &&
